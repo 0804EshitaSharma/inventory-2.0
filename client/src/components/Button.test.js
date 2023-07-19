d64f1826a6cbdc2c662d13c09524fcf14d64e610
eshitasharma <eshitasharma0804@gmail.com>
2023-07-19 14:54:49 -0700

    added-test

---
+++ client/src/components/Button.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Button from "./Button";
+
+test("renders button with correct label and type", () => {
+  const type = "submit";
+  const label = "Click Me";
+  const onClick = jest.fn();
+
+  const { getByText } = render(
+    <Button type={type} label={label} event={onClick} />
+  );
+
+  const buttonElement = getByText(label);
+  expect(buttonElement).toHaveTextContent(label);
+  expect(buttonElement).toHaveAttribute("type", type);
+  fireEvent.click(buttonElement);
+  expect(onClick).toHaveBeenCalledTimes(1);
+});
