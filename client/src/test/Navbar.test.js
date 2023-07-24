b0ebbdaf9df460dd5ec944306b5ed417a97c81fe
eshitasharma <eshitasharma0804@gmail.com>
2023-07-24 16:39:21 -0700

    added-tests

--- client/src/test/Navbar.test.js
+++ client/src/test/Navbar.test.js
@@ -1,40 +1,14 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
-// Create a mock store
 const middlewares = [];
 const mockStore = configureStore(middlewares);
 
-test("renders button with correct label and type", () => {
-  const initialState = {
-    items: { items: [] },
-  };
-  const store = mockStore(initialState);
-  const MockNavbar = () => {
-    return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <Navbar />
-        </BrowserRouter>
-      </Provider>
-    );
-  };
-
-  render(<MockNavbar />);
-   const homeLink = screen.getByText("Home");
-   expect(homeLink).toBeInTheDocument();
-
-   const aboutLink = screen.getByText("About");
-   expect(aboutLink).toBeInTheDocument();
-
-   const addLink = screen.getByText("Add");
-   expect(addLink).toBeInTheDocument();
-});
-test("renders button with correct label and type", () => {
+test("should render Navbar component properly", () => {
   const initialState = {
     items: { items: [] },
   };
@@ -52,10 +26,20 @@ test("renders button with correct label and type", () => {
   render(<MockNavbar />);
   const homeLink = screen.getByText("Home");
   expect(homeLink).toBeInTheDocument();
-
-  const aboutLink = screen.getByText("About");
-  expect(aboutLink).toBeInTheDocument();
-
+  fireEvent.click(homeLink);
+  expect(window.location.pathname).toBe("/");
   const addLink = screen.getByText("Add");
   expect(addLink).toBeInTheDocument();
+  fireEvent.click(addLink);
+  expect(window.location.pathname).toBe("/add");
+  const aboutLink = screen.getByText("About");
+  expect(aboutLink).toBeInTheDocument();
+  fireEvent.click(aboutLink);
+  expect(window.location.pathname).toBe("/about");
+  const searchBar = screen.getByRole("textbox");
+  expect(searchBar).toBeInTheDocument();
+  fireEvent.change(searchBar, {
+    target: { value: "Vegetables" },
+  });
+  expect(searchBar).toHaveValue("Vegetables");
 });
