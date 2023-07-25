f422c70b2720deb9638421ca21a8d9eba1f70311
eshitasharma <eshitasharma0804@gmail.com>
2023-07-24 19:23:08 -0700

    refactored-structure

--- client/src/test/ui/Modal.test.js
+++ client/src/test/ui/Modal.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import Modal from "../components/Modal";
+import Modal from "../../components/Modal";
 /* Referred from https://testing-library.com/docs/queries/about , https://www.youtube.com/watch?v=6wbnwsKrnYU and ChatGPT*/
 test("should render modal component properly", () => {
   const heading = "Notification";
